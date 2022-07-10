01c9639847b8b95a3def196c8aded5ba4420e9cb
chao <a0921342997@gmail.com>
2022-07-11 00:41:43 +0800

    Fixed Api router req , res missed type

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,7 +3,10 @@ import { getToken } from 'next-auth/jwt'
 
 const secret = process.env.SECRET
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   // if using `NEXTAUTH_SECRET` env variable, we detect it, and you won't actually need to `secret`
   // const token = await getToken({ req })
   // const token = await getToken({ req, secret })
