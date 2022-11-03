8c6f8fd2818bb5e74bb199d9f10ed729a8eb0999
chao <a0921342997@gmail.com>
2022-11-03 22:19:05 +0800

    test (Error: The Serverless Function api/getImageEmoji is 247.48mb which exceeds the maximum size limit of 50mb. Learn More: https://vercel.link/serverless-function-size)

--- pages/api/getImageEmoji.ts
+++ pages/api/getImageEmoji.ts
@@ -47,29 +47,29 @@ const tranferPath = (fullPath: string[]) => {
   // return pathObject
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    if (req.method === 'GET') {
-      const { pageIndex, limit } = req.query
-      if (!pageIndex) throw new Error('Please provide pageIndex')
-      const paths = await readFilePath(filePath)
-      if (!paths) {
-        res.status(401).json({ status: 'fail', message: 'File path not found' })
-      }
-      const slicePage = Number(pageIndex) * Number(limit)
-      const pathMap = tranferPath(paths as string[])
-      const result = pathMap.slice(slicePage - Number(limit), slicePage)
-      const isEnd = slicePage >= pathMap.length
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     if (req.method === 'GET') {
+//       const { pageIndex, limit } = req.query
+//       if (!pageIndex) throw new Error('Please provide pageIndex')
+//       const paths = await readFilePath(filePath)
+//       if (!paths) {
+//         res.status(401).json({ status: 'fail', message: 'File path not found' })
+//       }
+//       const slicePage = Number(pageIndex) * Number(limit)
+//       const pathMap = tranferPath(paths as string[])
+//       const result = pathMap.slice(slicePage - Number(limit), slicePage)
+//       const isEnd = slicePage >= pathMap.length
 
-      res
-        .status(200)
-        .json({ status: 'success', data: { emojiMap: [...result] }, isEnd })
-    } else res.status(400).json({ status: 'fail', message: 'Bad request' })
-  } catch (error) {
-    const { message } = error as Error
-    res.status(401).json({ status: 'fail', message: message })
-  }
-}
+//       res
+//         .status(200)
+//         .json({ status: 'success', data: { emojiMap: [...result] }, isEnd })
+//     } else res.status(400).json({ status: 'fail', message: 'Bad request' })
+//   } catch (error) {
+//     const { message } = error as Error
+//     res.status(401).json({ status: 'fail', message: message })
+//   }
+// }
