379ff936051e6e2d891c64b79055d7e769457f1c
chao <a0921342997@gmail.com>
2022-11-05 23:46:28 +0800

    Update (next js config file)

--- next.config.js
+++ next.config.js
@@ -10,6 +10,28 @@ const nextConfig = {
     minimumCacheTTL: 60 * 60 * 24,
     domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/public/static/emoji',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+      {
+        source: '/public/static/cover',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
