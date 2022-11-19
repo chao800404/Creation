0e0ba74b11b843a915005122c5f1b72824f48d1d
chao <a0921342997@gmail.com>
2022-11-19 10:20:06 +0800

    Update (middleware)

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
-import { rewrite } from '@vercel/edge'
 
 export default withAuth(
   function middleware(req) {
     const token = req.nextauth.token
+    console.log(token)
     if (token) {
       return NextResponse.rewrite(new URL('/dashboard', req.url))
     }
@@ -21,4 +21,4 @@ export default withAuth(
   }
 )
 
-export const config = { matcher: ['/', '/dashboard/:page*'] }
+export const config = { matcher: ['/'] }
